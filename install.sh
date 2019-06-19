7d22f0148b3175e5eca0506d0cfb6c39765c3a66
velais <velais@live.com>
2019-06-19 15:20:53 +0100

    Add .ideavimrc

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.vimrc.plug ~/.vimrc.plug
+ln -s ~/.dotfiles/.ideavimrc ~/.ideavimrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
 # Install zplugin
