3607fcddae82e255c400a0e9dad9cd147da22c19
velais <velais@live.com>
2019-06-23 19:04:54 +0100

    Add BrewFile

--- install.sh
+++ install.sh
@@ -6,6 +6,13 @@ ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.vimrc.plug ~/.vimrc.plug
 ln -s ~/.dotfiles/.ideavimrc ~/.ideavimrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.BrewFile ~/.BrewFile
+
+# Install brews
+brew bundle install --global
+# Run this to sync completely
+# brew bundle cleanup --global --force
 
 # Install zplugin
 git clone https://github.com/zdharma/zplugin.git "${HOME}/.zplugin/bin"
+
